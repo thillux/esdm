a11a4bba56db322ddbf1866ca5cec15524332a71
Markus Theil <theil.markus@gmail.com>
2025-07-09 13:24:03 +0200

    es_jent: enable NTG.1 mode
    Signed-off-by: Markus Theil <theil.markus@gmail.com>

--- esdm/esdm_es_jent.c
+++ esdm/esdm_es_jent.c
@@ -309,9 +309,7 @@ static int esdm_jent_initialize(void)
 	}
 
 	if (esdm_ntg1_2024_compliant()) {
-		flags |= JENT_FORCE_FIPS;
-		/* TODO: adapt, when jitterentropy became NTG.1 compliant */
-		/* flags |= JENT_NTG1 */
+		flags |= JENT_NTG1;
 	}
 
 	esdm_jent_state = jent_entropy_collector_alloc(0, flags);
